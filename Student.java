870aa0911560601b291ed71f3d6b17f4bfea5cae
smitkovska <56816739+smitkovska@users.noreply.github.com>
2020-04-12 19:46:44 +0200

    Add files via upload

--- Student.java
+++ Student.java
@@ -2,16 +2,52 @@ class Student {
 	String index;
 	String firstName;
 	String lastName;
-
+	int grades;
 	//TODO constructor
+Student (String index,String firstName,String lastName,int grades)
+{
+	index=index;
+	firstName=firstName;
+	lastName=lastName;
+	grades=grades;
+}
 
 	//TODO seters & getters
-
-	public double getAverage() {
+ public void setIndex(String index){
+            index = index;
+       
+        public void setFirstName(String f){
+            firstName = f;
+        }
+ public void setLastName(String l){
+            lastName = l;
+        }
+        public int getGrades(){
+            return grades;
+        }
+        public string index(){
+	return index;
+}
+        public string getFirstName(){
+            return firstName;
+	
+	public string getLastName(){
+return lastName;
+}
 		//TODO
+	for(i=0;i<get.grades();i++)
+	{
+		sum+=get.grades();
+	counter++;
 	}
+return sum/counter;
 
 	public int ECTSCredits() {
 		//TODO
+	for(i=0;i<get.grades();i++)
+{
+	if(get.grades()>5)
+	kredit=kredit+6;
 	}
+return kredit;
 }
